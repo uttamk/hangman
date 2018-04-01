eb3c2475865ccb6b95876e796138611d50901961
Uttam Kini <uttamkini@gmail.com>
2018-04-01 18:43:17 +0530

    Add scenario for already matched

--- hangman/lib/hangman/Game.ex
+++ hangman/lib/hangman/Game.ex
@@ -1,9 +1,34 @@
 defmodule Hangman.Game do
   defstruct(
+    letters: [],
     game_state: :initializing,
-    turns_left: 7
+    turns_left: 7,
+    used: MapSet.new()
   )
+
+  def new_game(word) do
+    %Hangman.Game{letters: word |> String.codepoints}
+  end
+
   def new_game() do
     %Hangman.Game{}
   end
+
+  def make_move(game, guess) do
+      {accept_move(game, guess, MapSet.member?(game.used, guess)), tally(game)}
+  end
+
+  def accept_move(game, guess,_already_used = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+
+  def accept_move(game, guess, _not_already_used) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
+  end
+
+  def tally(game) do
+    game
+  end
+
 end
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -1,11 +1,20 @@
 defmodule GameTest do
   use ExUnit.Case
   doctest Hangman.Game
+  alias Hangman.Game
 
   test "state when new game" do
-    game = Hangman.Game.new_game()
+    game = Game.new_game()
 
     assert game.game_state == :initializing
     assert game.turns_left == 7
+  end
+
+  test "state when same letter is guessed again" do
+    {game, _} = Game.new_game("hello") |> Game.make_move("h")
+    {game, _} = game |> Game.make_move("h")
+
+    assert game.game_state == :already_used
+
   end
 end
