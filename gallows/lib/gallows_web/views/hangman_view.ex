e124a73e247910d9ed0ede8e7a727b9484803cb8
Uttam Kini <uttamkini@gmail.com>
2018-04-26 12:09:33 +0530

    Add Gallows graphics

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -2,6 +2,13 @@ defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
   import Gallows.Views.Helpers.GameStateHelper
 
+  def turn(turns_left, target) when turns_left <= target do
+      "opacity: 1"
+  end
+
+  def turn(turns_left, target) do
+      "opacity: 0.1"
+  end
   def new_game(conn) do
     button("New Game", to: hangman_path(conn, :new_game))
   end
